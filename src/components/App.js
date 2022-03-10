5bc73424db3222073354cc8725b3fbc86725a519
Vaishali2127 <vaishalimahour8@gmail.com>
2022-03-10 12:10:57 +0530

    Add /chat

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,21 @@ import {
 } from "react-router-dom/cjs/react-router-dom.min";
 import "../index.css";
 
-// import Chats from './Chats'
+import { AuthProvider } from "../contexts/AuthContext";
+
+import Chats from "./Chats";
 import Login from "./Login";
 
 function App() {
   return (
     <div style={{ fontFamily: "Avenir" }}>
       <Router>
-        {/* <AuthPrider> */}
-        <Switch>
-          {/* <Route path="/chats" component={Chats}/> */}
-          <Route path="/" component={Login} />
-        </Switch>
-        {/* </AuthPrider> */}
+        <AuthProvider>
+          <Switch>
+            <Route path="/chats" component={Chats} />
+            <Route path="/" component={Login} />
+          </Switch>
+        </AuthProvider>
       </Router>
     </div>
   );
